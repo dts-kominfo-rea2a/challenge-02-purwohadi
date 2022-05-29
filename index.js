19bfcbc536b6126a70e5e0d6170c5d702a0352b7
purwo hadi <purwo_ok@yahoo.com>
2022-05-29 13:50:54 +0700

    Assesment Tantangan JS Alur Logika

--- index.js
+++ index.js
@@ -14,59 +14,79 @@
 const arrayObjectPegawai = require("./data-customer.json");
 
 function lakukanLooping(arrPegawai) {
-  // ! JANGAN DIMODIFIKASI
-  let dataYangAkanDilooping = arrPegawai;
+	// ! JANGAN DIMODIFIKASI
+	let dataYangAkanDilooping = arrPegawai;
 
-  /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
-      yang berisi gabungan nama depan dan belakang dari masing masing pegawai
+	/*
+		TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+		yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
-      Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
-  */
-  let hasilLooping = null;
+		Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
+	*/
+	let hasilLooping = [];
 
-  /*
-    TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
-      yang berisi jumlah pria dari masing masing pegawai
-  */
-  let jumlahPria = null;
+	/*
+		TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
+		yang berisi jumlah pria dari masing masing pegawai
+	*/
+	let jumlahPria = null;
 
-  /*
-    TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
-  */
-  let jumlahWanita = null;
+	/*
+		TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
+		yang berisi jumlah wanita dari masing masing pegawai
+	*/
+	let jumlahWanita = null;
 
-  /*
-    TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
-      
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
-  */
-  let komentar = null;
+	/*
+		TODO 4: Buatlah sebuah variabel bernama "komentar"
+		yang akan mengomentari apakah lebih banyak Pria atau Wanita
+		
+		Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+			"Jumlah Pria lebih banyak dari Wanita"
+		Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+			"Jumlah Wanita lebih banyak dari Pria"
+		Apabila imbang, maka komentar akan berisi:
+			"Jumlah Pria dan Wanita berimbang"
+	*/
+	let komentar = null;
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
+	/**Start here */
+	dataYangAkanDilooping.find((o, i) => {
+		hasilLooping.push(o.namaDepan+" "+o.namaBelakang)
+		if (o.jenisKelamin === 'M') jumlahPria ++
+		if (o.jenisKelamin === 'F') jumlahWanita ++
+	})
+	
+	if(jumlahPria > jumlahWanita)
+	{
+		komentar="Jumlah Pria lebih banyak dari Wanita"
+	}
+	else if(jumlahPria < jumlahWanita)
+	{
+		komentar="Jumlah Wanita lebih banyak dari Pria"
+	}
+	else
+	{
+		komentar="Jumlah Pria dan Wanita berimbang"
+	}
+
+	// ! JANGAN DIMODIFIKASI
+	return {
+		hasilLooping,
+		jumlahPria,
+		jumlahWanita,
+		komentar,
+	};
 }
 
 function main(data) {
-  const hasil = lakukanLooping(data || arrayObjectPegawai);
+  	const hasil = lakukanLooping(data || arrayObjectPegawai);
 
-  console.log(hasil.hasilLooping);
-  console.log(hasil.jumlahPria);
-  console.log(hasil.jumlahWanita);
+	console.log(hasil.hasilLooping);
+	console.log(hasil.jumlahPria);
+	console.log(hasil.jumlahWanita);
 
-  return hasil;
+	return hasil;
 }
 
 main(arrayObjectPegawai);
